a630cde6cec1fddf0fa26bbb45936d2a81de1e0b
myrear <mikuri.draque@gmail.com>
2020-01-14 11:27:16 +0900

    Update Preferences.tsx

--- src/Preferences.tsx
+++ src/Preferences.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { Grid, Card, Select, MenuItem, InputLabel, FormControl } from '@material-ui/core'
-import { StartupGachaKind, isStartupGachaKind } from './preference'
-import { STARTUP_GACHA_KIND_KEY } from './constants'
+import { StartupGachaKind, isStartupGachaKind, ChangingGachaBehavior } from './preference'
+import { STARTUP_GACHA_KIND_KEY, CHANGING_GACHA_BEHAVIOR_KEY } from './constants'
 
-const getDescription = (v: StartupGachaKind) => {
+const getGachaKindDescription = (v: StartupGachaKind) => {
     switch (v) {
         case StartupGachaKind.Dogiragon:
             return 'ドギラゴン・ガチャ'
@@ -15,40 +15,73 @@ const getDescription = (v: StartupGachaKind) => {
     }
 }
 
+const getChangingGachaBehaviorDescription = (v: ChangingGachaBehavior) => {
+    switch (v) {
+        case ChangingGachaBehavior.DoNotChange:
+            return '切り替えない'
+        case ChangingGachaBehavior.StopReelingAndForceChanging:
+            return 'ガチャを止め、切り替える'
+    }
+}
+
 export default () => {
+    const [startupGacha, setStartupGacha] = useState(StartupGachaKind.LastUsed)
+    const [changingGachaBehavior, setChangingGachaBehavior] = useState(ChangingGachaBehavior.DoNotChange)
+
     useEffect(() => {
         const savedStartupGacha = localStorage.getItem(STARTUP_GACHA_KIND_KEY)
         if (savedStartupGacha !== null) {
             const parsed = parseInt(savedStartupGacha)
             if (isStartupGachaKind(parsed)) {
                 setStartupGacha(parsed)
-                return;
             }
         }
     }, [])
-
-    const [startupGacha, setStartupGacha] = useState(StartupGachaKind.LastUsed)
 
     useEffect(() => {
         localStorage.setItem(STARTUP_GACHA_KIND_KEY, startupGacha.toString())
     }, [startupGacha])
 
-    const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-        setStartupGacha(event.target.value as StartupGachaKind)
-    };
+    useEffect(() => {
+        localStorage.setItem(CHANGING_GACHA_BEHAVIOR_KEY, changingGachaBehavior.toString())
+    }, [changingGachaBehavior])
+
+    const handleChange = function <T>(setState: React.Dispatch<React.SetStateAction<T>>) {
+        return (e: React.ChangeEvent<{ value: unknown }>) => {
+            setState(e.target.value as T)
+        }
+    }
 
     return (
         <Root>
-            <Grid container>
+            <Grid container spacing={1}>
                 <Grid item xs={12} sm={6}>
                     <Card>
                         <InnerCard>
-                            <FormControl>
+                            <FormControl fullWidth>
                                 <InputLabel>起動時のガチャ</InputLabel>
-                                <Select value={startupGacha} onChange={handleChange}>
-                                    <MenuItem value={StartupGachaKind.LastUsed}>{getDescription(StartupGachaKind.LastUsed)}</MenuItem>
-                                    <MenuItem value={StartupGachaKind.Dogiragon}>{getDescription(StartupGachaKind.Dogiragon)}</MenuItem>
-                                    <MenuItem value={StartupGachaKind.Dokindam}>{getDescription(StartupGachaKind.Dokindam)}</MenuItem>
+                                <Select value={startupGacha} onChange={handleChange<StartupGachaKind>(setStartupGacha)}>
+                                    {[StartupGachaKind.LastUsed, StartupGachaKind.Dogiragon, StartupGachaKind.Dokindam].map((v, i) => {
+                                        return (
+                                            <MenuItem value={v} key={i}>{getGachaKindDescription(v)}</MenuItem>
+                                        )
+                                    })}
+                                </Select>
+                            </FormControl>
+                        </InnerCard>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <Card>
+                        <InnerCard>
+                            <FormControl fullWidth>
+                                <InputLabel>ガチャ中の、ガチャ切替の挙動</InputLabel>
+                                <Select value={changingGachaBehavior} onChange={handleChange<ChangingGachaBehavior>(setChangingGachaBehavior)}>
+                                    {[ChangingGachaBehavior.DoNotChange, ChangingGachaBehavior.StopReelingAndForceChanging].map((v, i) => {
+                                        return (
+                                            <MenuItem value={v} key={i}>{getChangingGachaBehaviorDescription(v)}</MenuItem>
+                                        )
+                                    })}
                                 </Select>
                             </FormControl>
                         </InnerCard>
@@ -68,5 +101,6 @@ const InnerCard = styled.div`
     width: 100%;
     display: flex;
     margin: 16px 0;
+    padding: 0 16px;
     justify-content: center;
 `
