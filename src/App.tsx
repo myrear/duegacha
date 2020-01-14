b892060203bd696145b7f45850df146fa1f05623
myrear <mikuri.draque@gmail.com>
2020-01-14 11:35:42 +0900

    ガチャの処理をAppからMainに移動

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import styled, { keyframes } from 'styled-components';
-import { Appearance, DogiragonGachaAppearances, GachaKind, DokindamGachaAppearances } from './Gacha';
-import { REELING_DURATION, A_REEL_HEIGHT, LAST_USED_GACHA_KIND_KEY, STARTUP_GACHA_KIND_KEY } from "./constants";
-import './array.extension'
-import './number.exntension'
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import Main from './Main'
 import { Route, Switch } from 'react-router-dom'
 import { AppBar, Toolbar, Grid, IconButton, Typography } from '@material-ui/core';
 import Preferences from './Preferences';
 import { Menu } from "@material-ui/icons";
 import SideMenu from './SideMenu'
-import { StartupGachaKind } from './preference';
 
 const App = () => {
-  const [isAnimating, setIsAnimating] = useState(false)
-  const [animation, setAnimation] = useState(Reeling1)
-  const [appearance, setAppearance] = useState<Appearance | undefined>(undefined)
-  const [appearances, setAppearances] = useState(DogiragonGachaAppearances.concat([]).orderFromIndex(DogiragonGachaAppearances.length.randomInteger()))
-  const [kind, setKind] = useState<GachaKind | undefined>(undefined)
   const [isMenuOpened, setIsMenuOpened] = useState(false)
-
-  // 起動時のガチャ設定
-  useEffect(() => {
-    const savedKind = localStorage.getItem(LAST_USED_GACHA_KIND_KEY)
-    const savedStartupGacha = localStorage.getItem(STARTUP_GACHA_KIND_KEY)
-
-    if (savedStartupGacha === StartupGachaKind.Dokindam.toString()) { // ドキンダムガチャの場合
-      setKind(GachaKind.Dokindam)
-    } else if (savedStartupGacha === StartupGachaKind.Dogiragon.toString()) { // ドギラゴンガチャの場合
-      setKind(GachaKind.Dogiragon)
-    } else { // それ以外の場合、最後に使用したガチャの場合
-      if (savedKind === GachaKind.Dokindam.toString()) { // ドキンダムガチャを最後に使用していた場合
-        setKind(GachaKind.Dokindam)
-      } else setKind(GachaKind.Dogiragon) // それ以外の場合（デフォルト）
-    }
-  }, [])
-
-  // ガチャの種類が変更された時、種類に応じたガチャの出目に変更
-  useEffect(() => {
-    if (kind === undefined) return;
-    
-    localStorage.setItem(LAST_USED_GACHA_KIND_KEY, kind.toString())
-
-    if (kind === GachaKind.Dokindam) {
-      setAppearances(DokindamGachaAppearances.concat([]).orderFromIndex(DokindamGachaAppearances.length.randomInteger()))
-    } else setAppearances(DogiragonGachaAppearances.concat([]).orderFromIndex(DogiragonGachaAppearances.length.randomInteger()))
-    
-    setAppearance(undefined)
-  }, [kind])
-
-  // ガチャを回す時
-  const onPlayReeling = (): void => {
-    if (isAnimating) return;
-
-    setIsAnimating(true)
-
-    const index = appearances.length.randomInteger()
-    const newAppearances = appearances.orderFromIndex(index)
-    setAppearances(newAppearances)
-    const newAppearance = newAppearances[appearances.length - 3]
-    
-    window.setTimeout(() => {
-      setIsAnimating(false)
-      setAppearance(newAppearance)
-    }, REELING_DURATION)
-
-    if (animation === Reeling1) {
-      setAnimation(Reeling2)
-    } else {
-      setAnimation(Reeling1)
-    }
-  }
-
-  const onChangeGachaKind = (e: React.ChangeEvent<{}>, k: GachaKind) => {
-    if (isAnimating) return;
-    setKind(k)
-  }
 
   return (
     <Root>
@@ -99,7 +32,7 @@ const App = () => {
           <Switch>
             <Route exact path='/'>
               <MainWrapper>
-                <Main animation={animation} appearances={appearances} appearance={appearance} onPlayReeling={onPlayReeling} kind={kind} onChangeGachaKind={onChangeGachaKind} />
+                <Main />
               </MainWrapper>
             </Route>
             <Route path='/preferences'>
@@ -135,24 +68,6 @@ const MainWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: stretch;
-`
-
-const Reeling1 = keyframes`
-  from {
-    transform: translate3d(0, 0, 0);
-  }
-  to {
-    transform: translate3d(0, calc(-100% + ${A_REEL_HEIGHT * 4}px), 0)
-  }
-`
-
-const Reeling2 = keyframes`
-  0% {
-    transform: translate3d(0, 0, 0);
-  }
-  100% {
-    transform: translate3d(0, calc(-100% + ${A_REEL_HEIGHT * 4}px), 0)
-  }
 `
 
 export default App;
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,22 +1,75 @@
-import React from 'react'
-import styled, { Keyframes } from 'styled-components'
-import { Appearance, GachaKind } from './Gacha'
+import React, { useState, useEffect } from 'react'
+import styled, { Keyframes, keyframes } from 'styled-components'
+import { Appearance, GachaKind, DogiragonGachaAppearances, DokindamGachaAppearances } from './Gacha'
 import { Paper, IconButton, Grid, Typography, Tab, Tabs } from '@material-ui/core'
 import { Replay } from '@material-ui/icons'
 import Detail from './Detail'
-import { A_REEL_HEIGHT, REELING_DURATION } from "./constants";
+import { A_REEL_HEIGHT, REELING_DURATION, LAST_USED_GACHA_KIND_KEY, STARTUP_GACHA_KIND_KEY } from "./constants";
+import { StartupGachaKind } from "./preference";
 
-interface Props {
-    animation: Keyframes
-    appearances: Appearance[]
-    appearance?: Appearance
-    onPlayReeling: () => void
-    kind?: GachaKind
-    onChangeGachaKind: (e: React.ChangeEvent<{}>, newValue: GachaKind) => void
-}
+export default () => {
+    const [isAnimating, setIsAnimating] = useState(false)
+    const [animation, setAnimation] = useState(Reeling1)
+    const [appearance, setAppearance] = useState<Appearance | undefined>(undefined)
+    const [appearances, setAppearances] = useState(DogiragonGachaAppearances.concat([]).orderFromIndex(DogiragonGachaAppearances.length.randomInteger()))
+    const [kind, setKind] = useState<GachaKind | undefined>(undefined)
 
-export default (props: Props) => {
-    const { animation, appearance, appearances, onPlayReeling: onClick, kind, onChangeGachaKind } = props
+    // 起動時のガチャ設定
+    useEffect(() => {
+        const savedKind = localStorage.getItem(LAST_USED_GACHA_KIND_KEY)
+        const savedStartupGacha = localStorage.getItem(STARTUP_GACHA_KIND_KEY)
+
+        if (savedStartupGacha === StartupGachaKind.Dokindam.toString()) { // ドキンダムガチャの場合
+            setKind(GachaKind.Dokindam)
+        } else if (savedStartupGacha === StartupGachaKind.Dogiragon.toString()) { // ドギラゴンガチャの場合
+            setKind(GachaKind.Dogiragon)
+        } else { // それ以外の場合、最後に使用したガチャの場合
+            if (savedKind === GachaKind.Dokindam.toString()) { // ドキンダムガチャを最後に使用していた場合
+                setKind(GachaKind.Dokindam)
+            } else setKind(GachaKind.Dogiragon) // それ以外の場合（デフォルト）
+        }
+    }, [])
+
+    // ガチャの種類が変更された時、種類に応じたガチャの出目に変更
+    useEffect(() => {
+        if (kind === undefined) return;
+
+        localStorage.setItem(LAST_USED_GACHA_KIND_KEY, kind.toString())
+
+        if (kind === GachaKind.Dokindam) {
+            setAppearances(DokindamGachaAppearances.concat([]).orderFromIndex(DokindamGachaAppearances.length.randomInteger()))
+        } else setAppearances(DogiragonGachaAppearances.concat([]).orderFromIndex(DogiragonGachaAppearances.length.randomInteger()))
+
+        setAppearance(undefined)
+    }, [kind])
+
+    // ガチャを回す時
+    const onPlayReeling = (): void => {
+        if (isAnimating) return;
+
+        setIsAnimating(true)
+
+        const index = appearances.length.randomInteger()
+        const newAppearances = appearances.orderFromIndex(index)
+        setAppearances(newAppearances)
+        const newAppearance = newAppearances[appearances.length - 3]
+
+        window.setTimeout(() => {
+            setIsAnimating(false)
+            setAppearance(newAppearance)
+        }, REELING_DURATION)
+
+        if (animation === Reeling1) {
+            setAnimation(Reeling2)
+        } else {
+            setAnimation(Reeling1)
+        }
+    }
+
+    const onChangeGachaKind = (e: React.ChangeEvent<{}>, k: GachaKind) => {
+        if (isAnimating) return;
+        setKind(k)
+    }
 
     return (
         <Grid container spacing={2} alignItems={'stretch'} justify={'center'}>
@@ -46,7 +99,7 @@ export default (props: Props) => {
                                 }
                             </Items>
                         </ViewBox>
-                        <IconButton onClick={onClick}>
+                        <IconButton onClick={onPlayReeling}>
                             <Replay fontSize='large' />
                         </IconButton>
                     </ReelBox>
@@ -102,4 +155,22 @@ const Item = styled.div`
     box-sizing: border-box;
     height: ${A_REEL_HEIGHT}px;
     border-bottom: 1px solid #757575;
+`
+
+const Reeling1 = keyframes`
+  from {
+    transform: translate3d(0, 0, 0);
+  }
+  to {
+    transform: translate3d(0, calc(-100% + ${A_REEL_HEIGHT * 4}px), 0)
+  }
+`
+
+const Reeling2 = keyframes`
+  0% {
+    transform: translate3d(0, 0, 0);
+  }
+  100% {
+    transform: translate3d(0, calc(-100% + ${A_REEL_HEIGHT * 4}px), 0)
+  }
 `
